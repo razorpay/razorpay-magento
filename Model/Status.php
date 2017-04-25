71b5813609812da0350188169b0f9da5807ee062
Vivek Kumar <viveklakhera@gmail.com>
2017-04-25 16:58:52 +0530

    Add status class for default statuses

---
+++ Model/Status.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Razorpay\Magento\Model;
+
+/**
+ * Order Statuses source model
+ */
+class Status extends \Magento\Sales\Model\Config\Source\Order\Status
+{
+    /**
+     * @var null
+     */
+    protected $_stateStatuses = null;
+}
