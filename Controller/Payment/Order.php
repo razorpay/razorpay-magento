00a77f1451258ed83600a081381fdf52b67491b8
Seher Khan <47523938+seherkdc@users.noreply.github.com>
2019-02-22 13:10:47 +0530

    Update Order.php

--- Controller/Payment/Order.php
+++ Controller/Payment/Order.php
@@ -45,7 +45,7 @@ class Order extends \Razorpay\Magento\Controller\BaseController
     {
         $amount = (int) (round($this->getQuote()->getBaseGrandTotal(), 2) * 100);
 	    
-	$_order = $this->checkoutSession->getLastRealOrder();
+	$_order = $this->checkoutSession->getRealOrderId();
 	$receipt_id = $_order->getIncrementId();
 
         $code = 400;
