321464c0beaaa28b3874b46cbbcdc6d35ac4a6c4
Seher Khan <seher@kdc.in>
2019-03-11 17:15:40 +0530

    Update InstallSchema.php

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -3,13 +3,15 @@
 namespace Razorpay\Magento\Setup;
  
 use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
  
 class InstallSchema implements InstallSchemaInterface
 {
     public function install(
-        SchemaSetupInterface $setup
+        SchemaSetupInterface $setup,
+        ModuleContextInterface $context
     ) {
         $setup->getConnection()->addColumn(
                 $setup->getTable('sales_payment_transaction'),
