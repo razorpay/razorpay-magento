a80db7341aa70e1643870664b4c749087793a56b
Seher Khan <seher@kdc.in>
2019-03-11 17:45:25 +0530

    Update VerifyTransaction.php

--- Ui/Component/Listing/Columns/VerifyTransaction.php
+++ Ui/Component/Listing/Columns/VerifyTransaction.php
@@ -23,12 +23,7 @@ class Sendmail extends Column
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
  
-    /**
-     * Prepare Data Source
-     *
-     * @param array $dataSource
-     * @return array
-     **/
+    
     public function prepareDataSource(array $dataSource)
     {
         if (isset($dataSource['data']['items'])) {
