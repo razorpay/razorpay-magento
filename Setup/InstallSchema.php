c8e8d1930133b6c3d224ac41a5f1da1c792cae56
Seher Khan <seher@kdc.in>
2019-03-11 17:10:18 +0530

    Update InstallSchema.php

--- Setup/InstallSchema.php
+++ Setup/InstallSchema.php
@@ -1,10 +1,31 @@
-$setup->getConnection()->addColumn(
-        $setup->getTable('sales_payment_transaction'),
-        ‘verify_transaction',
-        [
-            'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            'length' => 255,
-            'nullable' => true,
-            'comment' => ‘Verify Transaction'
-        ]
-    );
+
+namespace Razorpay\Magento\Setup;
+ 
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+use Magento\Framework\App\Filesystem\DirectoryList;
+ 
+/**
+ * @codeCoverageIgnore
+ */
+class InstallSchema implements InstallSchemaInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @SuppressWarnings(PHPMD.ExcessiveMethodLength)
+     */
+    public function install(
+        SchemaSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $setup->getConnection()->addColumn(
+                $setup->getTable('sales_payment_transaction'),
+                ‘verify_transaction',
+                [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                        'comment' => ‘Verify Transaction'
+                ]
+        );
+    }
+}
