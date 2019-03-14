16b54101ac91902f2a3e0ba8513b014c53a113a2
Seher Khan <seher@kdc.in>
2019-03-14 11:52:20 +0530

    Create verify.js

---
+++ view/base/web/js/grid/columns/verify.js
@@ -0,0 +1,63 @@
+define([
+    'Magento_Ui/js/grid/columns/column',
+    'jquery',
+    'mage/template',
+    'text!razorpay-magento/templates/grid/cells/customer/verify.html',
+    'Magento_Ui/js/modal/modal'
+], function (Column, $, mageTemplate, sendmailPreviewTemplate) {
+    'use strict';
+ 
+    return Column.extend({
+        defaults: {
+            bodyTmpl: 'ui/grid/cells/html',
+            fieldClass: {
+                'data-grid-html-cell': true
+            }
+        },
+        gethtml: function (row) {
+            return row[this.index + '_html'];
+        },
+        getFormaction: function (row) {
+            return row[this.index + '_formaction'];
+        },
+        getCustomerid: function (row) {
+            return row[this.index + '_customerid'];
+        },
+        getLabel: function (row) {
+            return row[this.index + '_html']
+        },
+        getTitle: function (row) {
+            return row[this.index + '_title']
+        },
+        getSubmitlabel: function (row) {
+            return row[this.index + '_submitlabel']
+        },
+        getCancellabel: function (row) {
+            return row[this.index + '_cancellabel']
+        },
+        preview: function (row) {
+            var modalHtml = mageTemplate(
+                sendmailPreviewTemplate,
+                {
+                    html: this.gethtml(row), 
+                    title: this.getTitle(row), 
+                    label: this.getLabel(row), 
+                    formaction: this.getFormaction(row),
+                    customerid: this.getCustomerid(row),
+                    submitlabel: this.getSubmitlabel(row), 
+                    cancellabel: this.getCancellabel(row), 
+                    linkText: $.mage.__('Go to Details Page')
+                }
+            );
+            var previewPopup = $('<div/>').html(modalHtml);
+            previewPopup.modal({
+                title: this.getTitle(row),
+                innerScroll: true,
+                modalClass: '_image-box',
+                buttons: []}).trigger('openModal');
+        },
+        getFieldHandler: function (row) {
+            return this.preview.bind(this, row);
+        }
+    });
+});
