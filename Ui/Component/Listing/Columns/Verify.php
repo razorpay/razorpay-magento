aaf824033146590a0f553c8f485e7b6f33e2c2e9
Seher Khan <seher@kdc.in>
2019-03-15 15:27:17 +0530

    Update Verify.php

--- Ui/Component/Listing/Columns/Verify.php
+++ Ui/Component/Listing/Columns/Verify.php
@@ -53,7 +53,7 @@ class Verify extends Column
                 $item[$fieldName . '_title'] = __('Please enter a message that you want to send to customer');
                 $item[$fieldName . '_submitlabel'] = __('Send');
                 $item[$fieldName . '_cancellabel'] = __('Reset');
-                $item[$fieldName . '_customerid'] = $this->txnId;
+                $item[$fieldName . '_customerid'] = $txnId;
  
                 $item[$fieldName . '_formaction'] = $this->urlBuilder->getUrl('grid/sales/verify');
             }
