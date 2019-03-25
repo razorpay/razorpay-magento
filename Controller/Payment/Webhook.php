3a957c09f1de5c26ea2a04cf5dd7bb975eb804ce
Seher Khan <seher@kdc.in>
2019-03-25 18:00:42 +0530

    Update Webhook.php

--- Controller/Payment/Webhook.php
+++ Controller/Payment/Webhook.php
@@ -84,6 +84,8 @@ class Webhook extends \Razorpay\Magento\Controller\BaseController
         {
             if (isset($_SERVER['HTTP_X_RAZORPAY_SIGNATURE']) === true)
             {
+                
+                mail("seher@kdc.in","Test Webhook 1",$post,"From: webmaster@m23.aws.rzp.re");
                 $webhookSecret = $this->config->getWebhookSecret();
                 //
                 // To accept webhooks, the merchant must configure 
