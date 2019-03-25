bf4008ff6719e7fe2e8437c1d31c7a66486af83b
Seher Khan <seher@kdc.in>
2019-03-25 18:54:09 +0530

    Update Webhook.php

--- Controller/Payment/Webhook.php
+++ Controller/Payment/Webhook.php
@@ -1,74 +1,12 @@
 <?php 
 namespace Razorpay\Magento\Controller\Payment;
-use Razorpay\Api\Api;
-use Razorpay\Api\Errors;
-use Razorpay\Magento\Model\Config;
-use Razorpay\Magento\Model\PaymentMethod;
+
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Sales\Model\Order\Payment;
+
 class Webhook extends \Razorpay\Magento\Controller\BaseController
 {
-    /**
-     * @var \Magento\Checkout\Model\Session
-     */
-    protected $checkoutSession;
-    /**
-     * @var \Magento\Quote\Model\QuoteRepository
-     */
-    protected $quoteRepository;
-    /**
-     * @var \Magento\Sales\Api\Data\OrderInterface
-     */
-    protected $order;
-    protected $api;
-    protected $logger;
-    protected $quoteManagement;
-    protected $objectManagement;
-    protected $storeManager;
-    protected $customerRepository;
-    const STATUS_APPROVED = 'APPROVED';
-    /**
-     * @param \Magento\Framework\App\Action\Context $context
-     * @param \Magento\Customer\Model\Session $customerSession
-     * @param \Magento\Checkout\Model\Session $checkoutSession
-     * @param \Magento\Razorpay\Model\CheckoutFactory $checkoutFactory
-     * @param \Razorpay\Magento\Model\Config $config
-     * @param \Magento\Catalog\Model\Session $catalogSession
-     * @param \Magento\Quote\Model\QuoteRepository $quoteRepository,
-     * @param \Magento\Sales\Api\Data\OrderInterface $order
-     */
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Customer\Model\Session $customerSession,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Razorpay\Magento\Model\CheckoutFactory $checkoutFactory,
-        \Razorpay\Magento\Model\Config $config,
-        \Magento\Catalog\Model\Session $catalogSession,
-        \Magento\Quote\Model\QuoteRepository $quoteRepository,
-        \Magento\Sales\Api\Data\OrderInterface $order,
-        \Magento\Quote\Model\QuoteManagement $quoteManagement,
-        \Magento\Store\Model\StoreManagerInterface $storeManagement,
-        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
-        \Psr\Log\LoggerInterface $logger
-    ) 
-    {
-        parent::__construct(
-            $context,
-            $customerSession,
-            $checkoutSession,
-            $config
-        );
-        $keyId                 = $this->config->getConfigData(Config::KEY_PUBLIC_KEY);
-        $keySecret             = $this->config->getConfigData(Config::KEY_PRIVATE_KEY);
-        $this->api             = new Api($keyId, $keySecret);
-        $this->order           = $order;
-        $this->logger          = $logger;
-        $this->objectManagement   = \Magento\Framework\App\ObjectManager::getInstance();
-        $this->quoteManagement    = $quoteManagement;
-        $this->checkoutFactory    = $checkoutFactory;
-        $this->catalogSession     = $catalogSession;
-        $this->quoteRepository    = $quoteRepository;
-        $this->storeManagement    = $storeManagement;
-        $this->customerRepository = $customerRepository;
-    }
+    
     /**
      * Processes the incoming webhook
      */
