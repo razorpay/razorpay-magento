d0328bae40cad1eb67f52051de8710773b6b98a8
Seher Khan <seher@kdc.in>
2019-03-25 19:35:57 +0530

    Update Webhook.php

--- Controller/Payment/Webhook.php
+++ Controller/Payment/Webhook.php
@@ -1,7 +1,9 @@
 <?php
-$msg = 'Request'."\n";
-$msg .= var_dump($_REQUEST)."\n\n";
-$msg .= 'UA'."\n";
+$msg = 'Request:'."\n";
+$msg .= json_encode($_REQUEST)."\n\n";
+$msg .= 'UA:'."\n";
 $msg .= $_SERVER['HTTP_USER_AGENT']."\n\n-x-";
 mail("seher@kdc.in","WebHook Test ".mt_rand(100000,999999),"Request:".$msg);
+echo $msg;
+exit();
 ?>
