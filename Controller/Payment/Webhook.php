e19982600e52e711d4e280cdddd877eca6b1fe0d
Seher Khan <seher@kdc.in>
2019-03-27 16:21:53 +0530

    Update Webhook.php

--- Controller/Payment/Webhook.php
+++ Controller/Payment/Webhook.php
@@ -1,12 +1,29 @@
 <?php
-$request = file_get_contents('php://input');
-$msg = 'Request'."\n";
-$msg .= json_encode($request, true)."\n\n";
-$msg .= 'UA:'."\n";
-$msg .= $_SERVER['HTTP_USER_AGENT']."\n\n-x-";
-mail("seher@kdc.in","WebHook Test ".mt_rand(100000,999999),"Request:".$msg);
-echo "<pre>";
-print_r($msg);
-echo "</pre>";
-exit();
-?>
+
+namespace Razorpay\Magento\Controller\Payment;
+
+class Webhook extends \Razorpay\Magento\Controller\BaseController
+{
+    /** @var \Psr\Log\LoggerInterface  */
+    protected $logger;
+    /**
+    * @param \Magento\Framework\App\Action\Context $context,
+    * @param \Psr\Log\LoggerInterface $logger
+    */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Psr\Log\LoggerInterface $logger
+    ) {
+        $this->logger = $logger;
+      
+        parent::__construct($context);
+    }
+    public function execute()
+    {
+        $this->logger->addDebug('razorpay/payment/webhook endpoint was hit');
+        
+        $request = file_get_contents('php://input');
+        $msg = json_encode($request, true);
+        $this->logger->addDebug("Json Output: ".print_r($msg, true));
+    }
+}
