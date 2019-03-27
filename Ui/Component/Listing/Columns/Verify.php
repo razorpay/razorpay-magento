59501301a2c7a0c8a6a539bc3bdef47050bee483
Seher Khan <seher@kdc.in>
2019-03-27 12:16:28 +0530

    Update Verify.php

--- Ui/Component/Listing/Columns/Verify.php
+++ Ui/Component/Listing/Columns/Verify.php
@@ -6,10 +6,11 @@ use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\UiComponent\ContextInterface;
 use Magento\Framework\View\Element\UiComponentFactory;
 use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Sales\Model\ResourceModel\Order\Payment\Transaction\CollectionFactory;
 use Magento\Sales\Model\Order\Payment\Transaction;
 use Magento\Sales\Model\Order\Payment;
  
-class Verify extends Column
+class Verify extends Column 
 {
     /**
      * @var UrlInterface
@@ -31,12 +32,15 @@ class Verify extends Column
         ContextInterface $context,
         UiComponentFactory $uiComponentFactory,
         UrlInterface $urlBuilder,
+        CollectionFactory $collectionFactory,
         Payment $payment,
         Transaction $transaction,
         array $components = [],
         array $data = []
     ) {
         $this->urlBuilder = $urlBuilder;
+        $this->statuses = $collectionFactory->create()->toOptionHash();
+        $this->transactions = $collectionFactory->create()->addPaymentIdFilter($payment);
         $this->payment = $payment;
         $this->transaction = $transaction;
         parent::__construct($context, $uiComponentFactory, $components, $data);
@@ -58,7 +62,7 @@ class Verify extends Column
                 $item[$fieldName . '_title'] = __('Please enter a message that you want to send to customer');
                 $item[$fieldName . '_submitlabel'] = __('Send');
                 $item[$fieldName . '_cancellabel'] = __('Reset');
-                $item[$fieldName . '_customerid'] = $this->payment->getTransactionId();
+                $item[$fieldName . '_customerid'] = $this->transactions[$item[$this->getData('name')]];
  
                 $item[$fieldName . '_formaction'] = $this->urlBuilder->getUrl('grid/sales/verify');
             }
