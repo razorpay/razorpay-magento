77882c8f56e5dc349f22fe03a5869196d46a4a28
Seher Khan <seher@kdc.in>
2019-03-27 16:54:55 +0530

    Update Webhook.php

--- Controller/Payment/Webhook.php
+++ Controller/Payment/Webhook.php
@@ -7,19 +7,10 @@ class Webhook extends \Razorpay\Magento\Controller\BaseController
     /** @var \Psr\Log\LoggerInterface  */
     protected $logger;
     /**
-    * @param \Magento\Framework\App\Action\Context $context,
     * @param \Psr\Log\LoggerInterface $logger
     */
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context,
-        \Magento\Customer\Model\Session $customerSession,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Razorpay\Magento\Model\Config $config,
-        \Psr\Log\LoggerInterface $logger
-    ) {
+    public function __construct(\Psr\Log\LoggerInterface $logger) {
         $this->logger = $logger;
-      
-        parent::__construct($context,$customerSession,$checkoutSession,$config);
     }
     public function execute()
     {
