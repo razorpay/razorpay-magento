7fe5734e144239f723999284b343f9e71ac725e9
Seher Khan <seher@kdc.in>
2019-03-27 13:25:10 +0530

    Update Verify.php

--- Ui/Component/Listing/Columns/Verify.php
+++ Ui/Component/Listing/Columns/Verify.php
@@ -33,15 +33,11 @@ class Verify extends Column
         UiComponentFactory $uiComponentFactory,
         UrlInterface $urlBuilder,
         CollectionFactory $collectionFactory,
-        Payment $payment,
         Transaction $transaction,
         array $components = [],
         array $data = []
     ) {
         $this->urlBuilder = $urlBuilder;
-        $this->statuses = $collectionFactory->create()->toOptionHash();
-        $this->transactions = $collectionFactory->create()->addPaymentIdFilter($payment);
-        $this->payment = $payment;
         $this->transaction = $transaction;
         parent::__construct($context, $uiComponentFactory, $components, $data);
     }
@@ -62,7 +58,7 @@ class Verify extends Column
                 $item[$fieldName . '_title'] = __('Please enter a message that you want to send to customer');
                 $item[$fieldName . '_submitlabel'] = __('Send');
                 $item[$fieldName . '_cancellabel'] = __('Reset');
-                $item[$fieldName . '_customerid'] = $this->transactions[$item[$this->getData('name')]];
+                $item[$fieldName . '_customerid'] = $this->transaction->getTxnId();
  
                 $item[$fieldName . '_formaction'] = $this->urlBuilder->getUrl('grid/sales/verify');
             }
