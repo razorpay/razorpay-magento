caa95b427faefc887388630bbffe96ab8d4f4dc7
Seher Khan <seher@kdc.in>
2019-03-28 15:04:44 +0530

    Update Webhook.php

--- Controller/Payment/Webhook.php
+++ Controller/Payment/Webhook.php
@@ -2,8 +2,31 @@
 
 namespace Razorpay\Magento\Controller\Payment;
 
-class Webhook extends \Magento\Framework\App\Action\Action
+class Webhook extends \Razorpay\Magento\Controller\BaseController
 {
+	protected $checkoutSession;
+	/**
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Razorpay\Model\CheckoutFactory $checkoutFactory
+     * @param \Magento\Razorpay\Model\Config\Payment $razorpayConfig
+     */
+    public function __construct(
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Customer\Model\Session $customerSession,
+        \Magento\Checkout\Model\Session $checkoutSession,
+        \Razorpay\Magento\Model\Config $config
+    ) 
+    {
+        parent::__construct(
+            $context,
+            $customerSession,
+            $checkoutSession,
+            $config
+        );
+	    $this->config = $config;
+    }
     public function execute()
     {
         echo "hello"."\n\n";
