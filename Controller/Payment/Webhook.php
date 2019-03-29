01df49ea99ff4f85ce6f878ff4575329c5099379
Seher Khan <seher@kdc.in>
2019-03-29 17:24:19 +0530

    Update Webhook.php

--- Controller/Payment/Webhook.php
+++ Controller/Payment/Webhook.php
@@ -13,10 +13,13 @@ $resource = $objectManager->get('Magento\Framework\App\ResourceConnection');
 $connection = $resource->getConnection();
 
 $query1 = "SELECT order_id FROM sales_payment_transaction WHERE txn_id = 'pay_CDBwkdURcNaAHN'";
-$magento_orderId = $connection->fetchAll($query1); 
+$magento_or = $connection->fetchAll($query1); 
+
+$magento_orderId = $response1[0]['order_id'];
 
 //$query2 = "SELECT status FROM sales_order WHERE entity_id = '$magento_orderId'";
 //$magento_orderStatus = $connection->fetchAll($query2);
 
+echo '<pre>'; print_r($response); echo '</pre>';
 echo '<pre>'; print_r($magento_orderId); echo '</pre>';
 //echo '<pre>'; print_r($magento_orderStatus); echo '</pre>';
