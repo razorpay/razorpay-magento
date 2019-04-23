ba984f15f4db432eeac0e757973a08e33fcffae3
Seher Khan <seher@kdc.in>
2019-04-23 09:54:29 +0530

    Get Payment Action value (#135)
    Define constant for `KEY_PAYMENT_ACTION`
    Create `getPaymentAction()` function to retrieve payment action configured by the admin.

--- Model/Config.php
+++ Model/Config.php
@@ -12,6 +12,7 @@ class Config
     const KEY_PUBLIC_KEY = 'key_id';
     const KEY_PRIVATE_KEY = 'key_secret';
     const KEY_MERCHANT_NAME_OVERRIDE = 'merchant_name_override';
+    const KEY_PAYMENT_ACTION = 'payment_action';
 
     /**
      * @var string
@@ -48,6 +49,11 @@ class Config
     public function getKeyId()
     {
         return $this->getConfigData(self::KEY_PUBLIC_KEY);
+    }
+    
+    public function getPaymentAction()
+    {
+        return $this->getConfigData(self::KEY_PAYMENT_ACTION);
     }
 
     /**
