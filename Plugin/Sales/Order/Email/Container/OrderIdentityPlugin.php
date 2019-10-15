3c20ae00f7f010027b3fd894784ce6e10f82acfc
Ram Prakash Singh <ram.techchef@razorpay.com>
2019-10-15 15:45:57 +0530

    create magento order before the payment with pending payment status.

---
+++ Plugin/Sales/Order/Email/Container/OrderIdentityPlugin.php
@@ -0,0 +1,41 @@
+<?php
+namespace Razorpay\Magento\Plugin\Sales\Order\Email\Container;
+
+class OrderIdentityPlugin
+{
+    /**
+     * @var \Magento\Checkout\Model\Session $checkoutSession
+     */
+    protected $checkoutSession;
+
+    /**
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     *
+     * @codeCoverageIgnore
+     */
+    public function __construct(
+        \Magento\Checkout\Model\Session $checkoutSession
+    )
+    {
+        $this->checkoutSession = $checkoutSession;
+    }
+
+    /**
+     * @param \Magento\Sales\Model\Order\Email\Container\OrderIdentity $subject
+     * @param callable $proceed
+     * @return bool
+     */
+    public function aroundIsEnabled(\Magento\Sales\Model\Order\Email\Container\OrderIdentity $subject, callable $proceed)
+    {
+        $returnValue = $proceed();
+
+        $forceOrderMailSentOnSuccess = $this->checkoutSession->getRazorpayMailSentOnSuccess();
+        if(isset($forceOrderMailSentOnSuccess))
+        {
+            $returnValue = $forceOrderMailSentOnSuccess;
+            $this->checkoutSession->unsRazorpayMailSentOnSuccess();           
+        }
+        
+        return $returnValue;
+    }
+}
