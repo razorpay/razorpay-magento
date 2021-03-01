ecef728a9a2f11a5cc33fcf0e19bd274cfb53762
Ram Prakash Singh <ram.techchef@razorpay.com>
2021-03-01 20:41:17 +0530

    PON-970: removed catalog session and fixed duplicate quote entry in razorpay sales table

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -11,153 +11,127 @@ use Razorpay\Magento\Model\ResourceModel\OrderLink;
 
 class UpgradeSchema implements  UpgradeSchemaInterface
 {
-	public function upgrade(SchemaSetupInterface $setup,
-							ModuleContextInterface $context
-						)
-	{
-		$setup->startSetup();
+    public function upgrade(SchemaSetupInterface $setup,
+                            ModuleContextInterface $context
+                        )
+    {
+        $setup->startSetup();
 
-		if ((empty($context->getVersion()) === true) OR version_compare($context->getVersion(), '3.0.0', '>'))
-		{
-			// Get module table
-			$tableName = $setup->getTable('sales_order');
+        $table = $setup->getConnection()->newTable($setup->getTable(OrderLink::TABLE_NAME));
 
-			// Check if the table already exists
-			if ($setup->getConnection()->isTableExists($tableName) == true)
-			{
-				// Declare column definition
-				$definition =  [
-									'type' => \Magento\Framework\DB\Ddl\Table::TYPE_BOOLEAN,
-									'nullable' => false,
-									'comment' => 'Is placed by razorpay webhook',
-									'default' => 0
-								];
+        $table
+            ->addColumn(
+                'entity_id',
+                Table::TYPE_INTEGER,
+                null,
+                [
+                    'identity' => true,
+                    'unsigned' => true,
+                    'primary'  => true,
+                    'nullable' => false
+                ]
+            )
+            ->addColumn(
+                'quote_id',
+                Table::TYPE_INTEGER,
+                [
+                    'identity' => true,
+                    'unique'   => true,
+                    'nullable' => false
+                ]
+            )
+            ->addColumn(
+                'order_id',
+                Table::TYPE_INTEGER,
+                [
+                    'nullable' => true
+                ]
+            )
+            ->addColumn(
+                'increment_order_id',
+                Table::TYPE_TEXT,
+                32,
+                [
+                    'nullable' => true
+                ]
+            )
+            ->addColumn(
+                'rzp_order_id',
+                Table::TYPE_TEXT,
+                25,
+                [
+                    'nullable' => true
+                ]
+            )
+            ->addColumn(
+                'rzp_payment_id',
+                Table::TYPE_TEXT,
+                25,
+                [
+                    'nullable' => true
+                ]
+            )
+            ->addColumn(
+                'by_webhook',
+                Table::TYPE_BOOLEAN,
+                1,
+                [
+                    'nullable' => false,
+                    'default' => 0
+                ]
+            )
+            ->addColumn(
+                'by_frontend',
+                Table::TYPE_BOOLEAN,
+                1,
+                [
+                    'nullable' => false,
+                    'default' => 0
+                ]
+            )
+            ->addColumn(
+                'webhook_count',
+                Table::TYPE_SMALLINT,
+                3,
+                [
+                    'nullable' => false,
+                    'default' => 0
+                ]
+            )
+            ->addColumn(
+                'order_placed',
+                Table::TYPE_BOOLEAN,
+                1,
+                [
+                    'nullable' => false,
+                    'default' => 0
+                ]
+            )
+            ->addColumn(
+                'webhook_first_notified_at',
+                Table::TYPE_BIGINT,
+                [
+                    'nullable' => true
+                ]
+            )
+            ->addIndex(
+                'quote_id',
+                ['quote_id', 'rzp_payment_id'],
+                [
+                    'type'      => AdapterInterface::INDEX_TYPE_UNIQUE,
+                    'nullable'  => false,
+                ]
+            )
+            ->addIndex(
+                'increment_order_id',
+                ['increment_order_id'],
+                [
+                    'type'      => AdapterInterface::INDEX_TYPE_UNIQUE,
+                ]
+            );
 
-				$connection = $setup->getConnection();
+        $setup->getConnection()->createTable($table);
 
-				$connection->addColumn($tableName, 'by_razorpay_webhook', $definition);			 
-			}
-		}
-
-
-
-        if ((empty($context->getVersion()) === true) OR version_compare($context->getVersion(), '3.2.3', '>=')) {
-            $table = $setup->getConnection()->newTable($setup->getTable(OrderLink::TABLE_NAME));
-
-            $table
-                ->addColumn(
-                    'entity_id',
-                    Table::TYPE_INTEGER,
-                    null,
-                    [
-                        'identity' => true,
-                        'unsigned' => true,
-                        'primary'  => true,
-                        'nullable' => false
-                    ]
-                )
-                ->addColumn(
-                    'quote_id',
-                    Table::TYPE_INTEGER,
-                    [
-                        'identity' => true,
-                        'unique'   => true,
-                        'nullable' => false
-                    ]
-                )
-                ->addColumn(
-                    'order_id',
-                    Table::TYPE_INTEGER,
-                    [
-                        'nullable' => true
-                    ]
-                )
-                ->addColumn(
-                    'increment_order_id',
-                    Table::TYPE_TEXT,
-                    32,
-                    [
-                        'nullable' => true
-                    ]
-                )
-                ->addColumn(
-                    'rzp_order_id',
-                    Table::TYPE_TEXT,
-                    25,
-                    [
-                        'nullable' => true
-                    ]
-                )
-                ->addColumn(
-                    'rzp_payment_id',
-                    Table::TYPE_TEXT,
-                    25,
-                    [
-                        'nullable' => true
-                    ]
-                )
-                ->addColumn(
-                    'by_webhook',
-                    Table::TYPE_BOOLEAN,
-                    1,
-                    [
-                        'nullable' => false,
-                        'default' => 0
-                    ]
-                )
-                ->addColumn(
-                    'by_frontend',
-                    Table::TYPE_BOOLEAN,
-                    1,
-                    [
-                        'nullable' => false,
-                        'default' => 0
-                    ]
-                )
-                ->addColumn(
-                    'webhook_count',
-                    Table::TYPE_SMALLINT,
-                    3,
-                    [
-                        'nullable' => false,
-                        'default' => 0
-                    ]
-                )
-                ->addColumn(
-                    'order_placed',
-                    Table::TYPE_BOOLEAN,
-                    1,
-                    [
-                        'nullable' => false,
-                        'default' => 0
-                    ]
-                )
-                ->addColumn(
-                    'webhook_first_notified_at',
-                    Table::TYPE_BIGINT,
-                    [
-                        'nullable' => true
-                    ]
-                )
-                ->addIndex(
-			        'quote_id',
-			        ['quote_id', 'rzp_payment_id'],
-			        [
-						'type'		=> AdapterInterface::INDEX_TYPE_UNIQUE,
-						'nullable'  => false,
-					]
-				)
-				->addIndex(
-			        'increment_order_id',
-			        ['increment_order_id'],
-			        [
-						'type'		=> AdapterInterface::INDEX_TYPE_UNIQUE,
-					]
-				);
-
-            $setup->getConnection()->createTable($table);
-        }
-
-		$setup->endSetup();
-	}
+        $setup->endSetup();
+    }
 }
