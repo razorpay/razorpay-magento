441a134ad1868fadd2603c001ec4a546665d8027
Nishant Vaity <nishant.vaity@razorpay.com>
2022-01-31 18:00:05 +0530

    Added admin html settings for Webhook Events, Webhook URL, Webhook Config

---
+++ Model/WebhookEvents.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Razorpay\Magento\Model;
+
+use \Magento\Framework\Option\ArrayInterface;
+
+class WebhookEvents implements ArrayInterface
+{
+    /**
+     * @inheritdoc
+     */
+    public function toOptionArray()
+    {
+        return [
+            [
+                'value' => "order.paid",
+                'label' => __('order.paid'),
+            ],
+            [
+                'value' => "payment.authorized",
+                'label' => __('payment.authorized'),
+            ],
+        ];
+    }
+}
