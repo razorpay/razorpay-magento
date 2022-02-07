30d3591290a37f690d8c0e165c84866e2fcfb21e
Ram Prakash Singh <ramprakash.singh@razorpay.com>
2022-02-07 17:22:54 +0530

    webhook first addition on top of PR#320

---
+++ Plugin/CsrfValidatorSkip.php
@@ -0,0 +1,25 @@
+<?php
+namespace Razorpay\Magento\Plugin;
+
+class CsrfValidatorSkip
+{
+    /**
+     * CsrfValidator to skip validation for razorpay
+     *
+     * @param \Magento\Framework\App\Request\CsrfValidator $subject
+     * @param \Closure $proceed
+     * @param \Magento\Framework\App\RequestInterface $request
+     * @param \Magento\Framework\App\ActionInterface $action
+     */
+    public function aroundValidate(
+        $subject,
+        \Closure $proceed,
+        $request,
+        $action
+    ) {
+        if ($request->getModuleName() == 'razorpay') {
+            return; // Skip CSRF check
+        }
+        $proceed($request, $action); // Proceed Magento 2 core functionalities
+    }
+}
