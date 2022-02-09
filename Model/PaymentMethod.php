42f5594ec33cb32a277b683e4a013fc6ca10b5b9
Ram Prakash Singh <ramprakash.singh@razorpay.com>
2022-02-09 10:30:46 +0530

    Added refund with invoice creadit memo

--- Model/PaymentMethod.php
+++ Model/PaymentMethod.php
@@ -211,6 +211,55 @@ class PaymentMethod extends \Magento\Payment\Model\Method\AbstractMethod
      */
     public function refund(InfoInterface $payment, $amount)
     {
+        $order = $payment->getOrder();
+
+        $creditmemo = $this->request->getPost('creditmemo');
+
+        $reason = (!empty($creditmemo['comment_text'])) ? $creditmemo['comment_text'] : 'Refunded by site admin';
+
+        $refundId = $payment->getTransactionId();
+
+        $this->_logger->info('Razorpay Refund - Transaction ID:' . $refundId);
+
+        $paymentId = substr($refundId, 0, -7);
+
+        try
+        {
+            $data = [
+                'amount'    =>  (int) round($amount * 100),
+                'receipt'   =>  $order->getIncrementId(),
+                'notes'     =>  [
+                    'reason'                =>  $reason,
+                    'order_id'              =>  $order->getIncrementId(),
+                    'refund_from_website'   =>  true,
+                    'source'                =>  'Magento',
+                ]
+            ];
+
+            $refund = $this->rzp->payment
+                                ->fetch($paymentId)
+                                ->refund($data);
+
+            $payment->setAmountPaid($amount)
+                    ->setLastTransId($refund->id)
+                    ->setTransactionId($refund->id)
+                    ->setIsTransactionClosed(true)
+                    ->setShouldCloseParentTransaction(true);
+
+        }
+        catch (\Razorpay\Api\Errors\Error $e)
+        {
+            $this->_logger->critical($e);
+
+            throw new LocalizedException(__('Razorpay Error: %1.', $e->getMessage()));
+        }
+        catch (\Exception $e)
+        {
+            $this->_logger->critical($e);
+
+            throw new LocalizedException(__('Razorpay Error: %1.', $e->getMessage()));
+        }
+
         return $this;
     }
 
