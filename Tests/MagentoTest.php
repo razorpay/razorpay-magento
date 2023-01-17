dcfd097e4606761d0446d87ad447bf4890320f63
Nishant Vaity <nishant.vaity@razorpay.com>
2023-01-17 19:28:46 +0530

    Added test case

---
+++ Tests/MagentoTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class MagentoTest extends TestCase {
+    public function testEmpty(): array
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+}
