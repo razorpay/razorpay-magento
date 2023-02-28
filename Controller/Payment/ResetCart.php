a7dfe7b86ad5ad63d7547f915700a13c2363c9e0
Yash Tripathi <yash.tripathi@razorpay.com>
2023-02-28 14:21:32 +0530

    Added changes to ResetCart controller for testing.

--- Controller/Payment/ResetCart.php
+++ Controller/Payment/ResetCart.php
@@ -42,27 +42,31 @@ class ResetCart extends \Razorpay\Magento\Controller\BaseController
         $this->catalogSession  = $catalogSession;
         $this->config          = $config;
         $this->logger          = $logger;
+        $this->objectManager   = \Magento\Framework\App\ObjectManager::getInstance();
     }
 
     public function execute()
     {
+        // @codeCoverageIgnoreStart
         $this->logger->info("Reset Cart started.");
-
+        // @codeCoverageIgnoreEnd
         $lastQuoteId = $this->checkoutSession->getLastQuoteId();
 
         $lastOrderId = $this->checkoutSession->getLastRealOrder();
 
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
+        //$objectManager = \Magento\Framework\App\ObjectManager::getInstance();
 
         if ($lastQuoteId && $lastOrderId)
         {
+            // @codeCoverageIgnoreStart
             $this->logger->info("Reset Cart: with lastQuoteId:" . $lastQuoteId);
+            // @codeCoverageIgnoreEnd
 
-            $orderModel = $objectManager->get('Magento\Sales\Model\Order')->load($lastOrderId->getEntityId());
+            $orderModel = $this->objectManager->get('Magento\Sales\Model\Order')->load($lastOrderId->getEntityId());
 
             if ($orderModel->canCancel())
             {
-                $quote = $objectManager->get('Magento\Quote\Model\Quote')->load($lastQuoteId);
+                $quote = $this->objectManager->get('Magento\Quote\Model\Quote')->load($lastQuoteId);
 
                 $quote->setIsActive(true)->save();
 
@@ -76,7 +80,9 @@ class ResetCart extends \Razorpay\Magento\Controller\BaseController
 
                 $this->checkoutSession->setFirstTimeChk('0');
 
+                // @codeCoverageIgnoreStart
                 $this->logger->info("Reset Cart: redirect_url: checkout/#payment");
+                // @codeCoverageIgnoreEnd
 
                 $responseContent = [
                     'success'           => true,
@@ -87,7 +93,9 @@ class ResetCart extends \Razorpay\Magento\Controller\BaseController
        
         if (!$lastQuoteId || !$lastOrderId)
         {
+            // @codeCoverageIgnoreStart
             $this->logger->info("Reset Cart: redirect_url: checkout/cart");
+            // @codeCoverageIgnoreEnd
 
             $responseContent = [
                 'success'           => true,
@@ -97,14 +105,16 @@ class ResetCart extends \Razorpay\Magento\Controller\BaseController
 
         $this->messageManager->addError(__('Payment Failed or Payment closed'));
 
+        // @codeCoverageIgnoreStart
         $this->logger->critical("Reset Cart: Payment Failed or Payment closed");
+        // @codeCoverageIgnoreEnd
 
         $response = $this->resultFactory->create(ResultFactory::TYPE_JSON);
 
         $response->setData($responseContent);
 
         $response->setHttpResponseCode(200);
-
+        
         return $response;
 
     }
