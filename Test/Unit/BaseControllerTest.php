3e3519e560589a29f1791161b00d12c64c1b7259
Nishant Vaity <nishant.vaity@razorpay.com>
2023-03-24 19:20:02 +0530

    Updated changes suggested at pr

--- Test/Unit/BaseControllerTest.php
+++ Test/Unit/BaseControllerTest.php
@@ -11,28 +11,34 @@ class BaseControllerTest extends TestCase
         $this->context = \Mockery::mock(
             \Magento\Framework\App\Action\Context::class
         )->makePartial()->shouldAllowMockingProtectedMethods();
+
         $this->customerSession = $this->createMock(
             \Magento\Customer\Model\Session::class
         );
+
         $this->checkoutSession = \Mockery::mock(
             \Magento\Checkout\Model\Session::class
         );
+
         $this->config = \Mockery::mock(
             \Razorpay\Magento\Model\Config::class
         );
+
         $this->quoteModel = \Mockery::mock(
             \Magento\Quote\Model\Quote::class
         );
+
         $this->checkoutFactory = \Mockery::mock(
             \Razorpay\Magento\Model\CheckoutFactory::class
         );
+
         $this->checkoutModel = \Mockery::mock(
             \Razorpay\Magento\Model\Checkout::class
         );
+
         $this->response = \Mockery::mock(
             \Magento\Framework\App\ResponseInterface::class
         );
-        
 
         $this->config->shouldReceive('getConfigData')->with('key_id')->andReturn('key_id');
         $this->config->shouldReceive('getConfigData')->with('key_secret')->andReturn('key_secret');
@@ -45,29 +51,36 @@ class BaseControllerTest extends TestCase
 
         $this->checkoutFactory->shouldReceive('create')->andReturn($this->checkoutModel);
 
-        $this->baseController = \Mockery::mock(Razorpay\Magento\Controller\BaseController::class, [ $this->context,
+        $this->baseController = \Mockery::mock(Razorpay\Magento\Controller\BaseController::class, [$this->context,
                                                                                                     $this->customerSession,
                                                                                                     $this->checkoutSession,
                                                                                                     $this->config])->makePartial()->shouldAllowMockingProtectedMethods();
-        
     }
+
     function getProperty($object, $propertyName, $value)
     {
         $reflection = new \ReflectionClass($object);
+
         $property = $reflection->getProperty($propertyName);
+
         $property->setAccessible(true);
         $property->setValue($object, $value);
+
         return $property->getValue($object);
     }
+
     public function testGetQuote()
     {
         $this->assertSame($this->quoteModel, $this->baseController->getQuote());
     }
+
     public function testGetCheckout()
     {
         $this->getProperty($this->baseController, 'checkoutFactory', $this->checkoutFactory);
+
         $this->assertSame($this->checkoutModel, $this->baseController->getCheckout());
     }
+
     public function testInitCheckout()
     {
         $this->quoteModel->shouldReceive('hasItems')->andReturn(true);
@@ -77,6 +90,5 @@ class BaseControllerTest extends TestCase
         $this->expectExceptionMessage("We can't initialize checkout.");
 
         $this->baseController->initCheckout();
-        
     }
 }
